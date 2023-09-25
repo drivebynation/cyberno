f206285e5fd6f75fa900273880a52df877fffdb1
drivebynation <ghanematin@gmail.com>
2023-09-26 01:31:24 +0330

    Edit rest-framework to rest_framework and add core app to INSTALLED_APPS

--- cyberno/settings.py
+++ cyberno/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest-framework',
+    'rest_framework',
+    'core.apps.CoreConfig'
 ]
 
 MIDDLEWARE = [
