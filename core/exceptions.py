8ca51fa2a32e9d5b915c66c56c376628289ddfd5
drivebynation <ghanematin@gmail.com>
2023-09-26 01:30:40 +0330

    Add some additional custom exceptions

--- core/exceptions.py
+++ core/exceptions.py
@@ -1,5 +1,18 @@
 class ExistError(Exception):
     """
-    User this Error When Entered string exists in queue
+    Use this Error When Entered string exists in queue
+    """
+    pass
+
+
+class ArrivedTurn(Exception):
+    """
+    Use this error when an arrived turn item asked
+    """
+    pass
+
+class EmptyQueueError(Exception):
+    """
+    Use this Error for Empty Queue
     """
     pass
