fea32b1b4109fff3ddc1389b79ebfdf2694f8337
drivebynation <ghanematin@gmail.com>
2023-09-26 01:34:19 +0330

    Add some view snippets for implementation our desired API views

--- core/views.py
+++ core/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from time import sleep
+from .models import Item
+import schedule
 
-# Create your views here.
+@api_view()
+def stack_fill(request, *args, **kwargs):
+    while 1:
+        schedule.every(20).seconds.do(Item.remove())
+        string = input("please Enter Your Item for queue: ")
+        Item.add(string)
+        sleep(20)
+
