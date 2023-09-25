3a0e9fd28788042e0c2403e59112d9b57a8e7829
drivebynation <ghanematin@gmail.com>
2023-09-25 18:27:38 +0330

    Add a new model called Item

--- core/models.py
+++ core/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.utils.translation import gettext as _
 
-# Create your models here.
+
+class Item(models.Model):
+    value = models.CharField(verbose_name=_("Value"), max_length=255)
+    turn = models.BigIntegerField(verbose_name=_("Turn"), null=True, blank=True)
+    passed = models.BooleanField(verbose_name=_("Passed"), default=False, blank=True)
+
+    class Meta:
+        verbose_name = _("Item")
+        verbose_name_plural = _("Items")
